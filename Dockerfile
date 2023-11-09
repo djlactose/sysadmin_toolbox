d1366adae1aee84c8c2b1e56fc84f738ab672e08
Nick Hernandez <nick.hernandez@zydoc.com>
2023-11-09 13:31:00 -0500

    Added the reinstalling of packages on container restart.

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,10 @@ FROM alpine
 VOLUME /mnt
 
 COPY run.sh /root/run.sh
-COPY utils/ root/utils/
+COPY start.sh /root/start.sh
+COPY utils/ /root/utils/
 
 RUN apk add --no-cache bash dialog && \
 ln -s /root/utils /mnt/utils
+
+ENTRYPOINT /root/start.sh
--- run.sh
+++ run.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
-choices=()
-for i in `find -L /mnt/ -mindepth 2 -maxdepth 2 -name name`; do 
-    choices+=("$(echo $i | cut -d "/" -f 3)" "$(cat $i)")
-done
-echo ${choices[@]}
-exitstatus=0
-while [ $exitstatus -eq 0 ]; do
-    OPTION=$(dialog --clear --title "System Administrator Toolbox" --menu "Select which script to run:" 15 30 5 "${choices[@]}" 2>&1 > /dev/tty)
-    exitstatus=$?
-    if [ $exitstatus = 0 ]; then
-        clear
-        $(./$OPTION/run.sh)
-    else
-        exitstatus=1
-        clear
-    fi
-done
+
+if [ -f /mnt/installed.txt ]; then
+    cat /mnt/installed.txt | bash
+fi
+sleep infinity
