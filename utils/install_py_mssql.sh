d393e86c9d8f5ee03fd0b5c2c6aaadae8b658899
Nick Hernandez <nick.hernandez@zydoc.com>
2023-11-09 13:55:40 -0500

    Added python3 mssql installer.

---
+++ utils/install_py_mssql.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+. $(dirname $0)/utils.conf
+
+wget https://download.microsoft.com/download/3/5/5/355d7943-a338-41a7-858d-53b259ea33f5/msodbcsql18_18.3.2.1-1_amd64.apk -O /tmp/msodbcsql18_18.3.2.1-1_amd64.apk
+wget https://download.microsoft.com/download/3/5/5/355d7943-a338-41a7-858d-53b259ea33f5/mssql-tools18_18.3.1.1-1_amd64.apk -O /tmp/mssql-tools18_18.3.1.1-1_amd64.apk
+echo \n | apk add --no-cache --allow-untrusted /tmp/msodbcsql18_18.3.2.1-1_amd64.apk /tmp/mssql-tools18_18.3.1.1-1_amd64.apk
+apk add --no-cache python3-dev py3-pip gcc libc-dev g++ libffi-dev libxml2 unixodbc-dev
+python3 -m pip install pyodbc pyopenssl
+rm /tmp/msodbcsql18_18.3.2.1-1_amd64.apk
+rm /tmp/mssql-tools18_18.3.1.1-1_amd64.apk
--- utils/run.sh
+++ utils/run.sh
@@ -8,11 +8,12 @@ choices+=("install_ansible.sh" "Install Ansible")
 choices+=("install_git.sh" "Install Git")
 choices+=("install_pwsh.sh" "Install Powershell")
 choices+=("install_py3.sh" "Install Python3")
+choices+=("install_py_mssql.sh" "Install Python3 libs for MSSQL")
 #######################################################################
 #######################################################################
 exitstatus=0
 while [ $exitstatus -eq 0 ]; do
-    OPTION=$(dialog --clear --title "Toolbox Utilities" --menu "Select which script to run:" 15 30 5 "${choices[@]}" 2>&1 > /dev/tty)
+    OPTION=$(dialog --clear --title "Toolbox Utilities" --menu "Select which script to run:" 15 100 5 "${choices[@]}" 2>&1 > /dev/tty)
     exitstatus=$?
     if [ $exitstatus -eq 0 ]; then
         clear
