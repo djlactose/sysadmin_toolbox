1c0f7994b285506eb77a2744b886eab49c498a2b
Nick Hernandez <nick.hernandez@zydoc.com>
2023-11-20 10:25:17 -0500

    Updated fix_perm.sh to include any added scripts

--- utils/other/change_pass.sh
+++ utils/other/change_pass.sh
@@ -1,7 +1,6 @@
 echo "NOTE: When you change the password of a user other than yourself it will ask them to change it upon login."
 echo ""
 read -p "Enter the username for password change (leave blank to change your own): "
-echo "\"$REPLY\""
 if [ "$REPLY" = "" ]; then
     passwd
 else
--- utils/other/fix_perm.sh
+++ utils/other/fix_perm.sh
@@ -2,7 +2,8 @@
 path=$( dirname "${BASH_SOURCE[0]}" )
 
 echo "Marking all utils as executable..."
-find $path/*.sh -exec chmod -v +x {} \;
+find $path/ -name '*.sh' -exec chmod -v +x {} \;
+find /mnt/ -name '*.sh' -exec chmod -v +x {} \;
 if [ -f /mnt/installed.txt ]; then
     chmod -v 744 /mnt/installed.txt
 fi
